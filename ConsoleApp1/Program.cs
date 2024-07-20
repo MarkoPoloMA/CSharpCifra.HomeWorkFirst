81914d5414ad1f46492034e64b16c52eb0dfd246
MarkoPoloMA <mark.utrobin@mail.ru>
2024-07-20 13:07:04 +0400

    FirstCommit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello World");
+            System("pause");
+        }
+
+        private static void System(string v)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
